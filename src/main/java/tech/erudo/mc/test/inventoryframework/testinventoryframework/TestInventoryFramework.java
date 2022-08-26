1ff8f2068db03da9c1cc660218defe579e3172a2
hirotask <hirotaskhashi@gmail.com>
2022-08-26 17:41:05 +0900

    first commit

---
+++ src/main/java/tech/erudo/mc/test/inventoryframework/testinventoryframework/TestInventoryFramework.java
@@ -0,0 +1,17 @@
+package tech.erudo.mc.test.inventoryframework.testinventoryframework;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TestInventoryFramework extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
